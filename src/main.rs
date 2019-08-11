3c78f140f75ad157129c79d322fa65255ad9ab03
Yuki Kodama <endflow.net@gmail.com>
2019-08-11 15:21:00 +0900

    First commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+fn handler0() {
+    println!("handler[0]");
+}
+
+fn handler1(a: usize) {
+    println!("handler[1]: {}", a);
+}
+
+fn handler1s(a: &str) {
+    println!("handler[1s]: {}", a);
+}
+
+fn handler2(a: usize, b: usize) {
+    println!("handler[2]: {}, {}", a, b);
+}
+
+trait FlexHandler<T, R> {
+    fn call(&self, param: T) -> R;
+}
+
+impl<F, R> FlexHandler<(), R> for F
+where
+    F: Fn() -> R
+{
+    fn call(&self, _param: ()) -> R {
+        (self)()
+    }
+}
+
+impl<F, R, A> FlexHandler<(A,), R> for F
+where
+    F: Fn(A) -> R
+{
+    fn call(&self, param: (A,)) -> R {
+        (self)(param.0)
+    }
+}
+
+fn main() {
+    let f0 = handler0;
+    f0();
+    f0.call(());
+
+    let f1 = handler1;
+    f1(42);
+    f1.call((42,));
+
+    let f1s = handler1s;
+    f1s("x");
+    f1s.call(("x",));
+}
